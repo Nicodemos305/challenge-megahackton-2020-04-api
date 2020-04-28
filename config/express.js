e277f0e6db9065e63b65a88fe3b7133874f4eae4
Bruno Augusto <bruno.sidf@gmail.com>
2020-04-28 20:40:09 -0300

    JWT

--- config/express.js
+++ config/express.js
@@ -2,12 +2,18 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const config     = require('config');
 const userRoute = require('../api/routes/user/usersRoute');
+const sessionController = require('../api/controllers/sessions/sessionController');
+const cors = require('cors')
 
 module.exports = () => {
   const app = express();
 
   app.set('port', process.env.PORT || config.get('server.port'));
+  app.use(cors());
   app.use(bodyParser.json());
+
+  app.post('/api/v1/sessions', sessionController.store);
+
   userRoute(app);
   // require('../api/routes/user/usersRoute')(app);
   require('../api/routes/goals/goalsRoute')(app);
