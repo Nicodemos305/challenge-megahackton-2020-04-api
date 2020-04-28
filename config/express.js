6d0f35340b8c26fde3b693479b7744524a52c54d
Bruno Augusto <bruno.sidf@gmail.com>
2020-04-27 23:13:10 -0300

    'nodemon'

--- config/express.js
+++ config/express.js
@@ -1,13 +1,15 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const config     = require('config');
+const userRoute = require('../api/routes/user/usersRoute');
 
 module.exports = () => {
   const app = express();
 
   app.set('port', process.env.PORT || config.get('server.port'));
   app.use(bodyParser.json());
-  require('../api/routes/user/usersRoute')(app);
+  userRoute(app);
+  // require('../api/routes/user/usersRoute')(app);
   require('../api/routes/goals/goalsRoute')(app);
 
   return app;
